a88a4ab0c9388cc75959260104f357a7be0428f2
proskurekov <johndoe@example.com>
2025-10-11 11:21:42 +0700

    update ruby course

--- courses/ruby/getting_started/hello-world/test.rb
+++ courses/ruby/getting_started/hello-world/test.rb
@@ -1,7 +1,7 @@
 result = hello()
 expected = 'Hello, World!'
 if result != expected
-  puts "Получено: #{expected}, ожидалось: #{result}"
+  puts "Получено: #{result}, ожидалось: #{expected}"
   exit 1
 end
 puts 'OK'
